791060196a9965d2ccb06420140bd2befbc96c02
Jefrysam <132527911+Jefrysam@users.noreply.github.com>
2024-03-20 21:29:49 -0500

    Add files via upload

---
+++ Js/menu.js
@@ -0,0 +1,13 @@
+var Precios = []
+
+
+
+document.getElementById("buttonEnviar").addEventListener('click', function() {
+    document.getElementById('formContainer').style.display = 'flex';
+});
+document.getElementById("buttonEnviar").addEventListener('click', function() {
+    document.getElementById("Menu01").style.display = 'none';   
+});
+document.getElementById("buttonEnviar").addEventListener('click', function() {    
+    document.getElementById("botonEnviar").style.display ="none";
+});
